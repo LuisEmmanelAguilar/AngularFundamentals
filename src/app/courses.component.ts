262b5ec981505c5cbeddb317b69ba7ff751b1d5a
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-02-08 17:23:16 -0600

    Directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,14 +2,19 @@ import {Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `
+                <h2>{{ title }}</h2>
+                <ul>
+                    <!--Directive *ngFor (one of most)-->
+                    <li *ngFor="let course of courses"> 
+                        {{ course }}
+                    </li>
+                </ul>
+    `
 })
 
 export class CoursesComponent {
     title ="List of courses";
-
-    getTitle() {
-        return this.title
-    }
+    courses= ["Course1", "Course2", "Course3"];
 
 }
