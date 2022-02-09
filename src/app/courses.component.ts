29e1b708c6c3008d4a67b9133ccb1f8e89b4c823
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-02-08 23:59:45 -0600

    Property Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,22 +4,15 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     template: `
-                <h2>{{ title }}</h2>
-                <ul>
-                    <!--Directive *ngFor (one of most)-->
-                    <li *ngFor="let course of courses"> 
-                        {{ course }}
-                    </li>
-                </ul>
-    `
+                <h2>{{ title }}</h2><!--String Interpolation-->
+                <h2 [textContent]="title"></h2><!--Property Binding-->
+
+                <img src="{{ imageUrl }}"/>
+                <img [src]="title" />
+            `
 })
 
 export class CoursesComponent {
     title ="List of courses";
-    courses;
-
-    // Dicoupled the class passing as a parameter to the constructor
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
-    }
+    imageUrl = "http//lorempixel.com/400/200";
 }
