58242ccb89b6a593035a6bfd31ecc3e89ac89572
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-02-09 00:29:25 -0600

    Attribute Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,15 +4,16 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     template: `
-                <h2>{{ title }}</h2><!--String Interpolation-->
-                <h2 [textContent]="title"></h2><!--Property Binding-->
-
-                <img src="{{ imageUrl }}"/>
-                <img [src]="title" />
-            `
+            <img [src]="imageUrl"] />
+            <table>
+                <tr>
+                    <td [attr.colspan]="colSpan"]> </td>
+                </tr>
+            </table>
+`
 })
 
 export class CoursesComponent {
-    title ="List of courses";
     imageUrl = "http//lorempixel.com/400/200";
+    colSpan = 2;
 }
