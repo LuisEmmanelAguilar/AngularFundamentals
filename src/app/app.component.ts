1ce8766c59a9dc679c0e7279a77221a9ccd72501
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-02-10 14:41:06 -0600

    Input Properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string;
 
-  constructor(){
-    this.title = "";
+  post = {
+    title: "Title",
+    isFavorite: false
   }
+
 }
