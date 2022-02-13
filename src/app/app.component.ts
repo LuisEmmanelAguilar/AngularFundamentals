8327c38cf69c6c9c9a0e466128b4c2374ef3860c
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-02-13 15:02:55 -0600

    Dierective ngFor and Track By

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'}
-  ];
+    { id:0, name:""},
+];
+
+  trackCourse(index:any, course:any){
+    return course ? course.id : undefined;
+  } 
+
+
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'course1'},
+      { id: 2, name: 'course2'},
+      { id: 3, name: 'course3'}
+    ];
+  }
 
   onAdd() {
     this.courses.push({ id: 4, name: 'course4'});
