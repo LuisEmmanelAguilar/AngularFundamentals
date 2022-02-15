dedbf67ea78f74ea25ab6f47953fb6537734130d
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-02-15 14:31:04 -0600

    Forms with droplist, radio buttons, input fields and Submit method form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent  {
 
+  contactMethods = [
+    {id: 1, name:"Email"},
+    {id: 2, name:"Phone"},
+    {id: 3, name:"Other"}
+  ];
+
   log(x:any) { console.log(x); }
 
+  submit(f:any) {
+    console.log(f);
+  }
   
 }
